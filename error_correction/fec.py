3d82e004a31959e309d0a98930a5745be221ec58
AnshKhurana <anshkhurana1507@gmail.com>
2019-01-29 14:10:40 +0530

    Basic layout

---
+++ error_correction/fec.py
@@ -0,0 +1,12 @@
+def encode(x):
+    pass
+
+def decode(x):
+    pass
+
+
+if __name__ == "__main__":
+    x = input("Enter string to encode: ")
+    y = decode(x)
+    print("Decode string: " + x)
+    
