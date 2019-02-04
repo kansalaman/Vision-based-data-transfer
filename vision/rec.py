6e114068a7c2f899e0acf6cf90f92b26bab19aa7
AnshKhurana <anshkhurana1507@gmail.com>
2019-02-04 16:59:05 +0530

    Detection working

--- vision/rec.py
+++ vision/rec.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import numpy as np
 
 if __name__ == '__main__':
 
@@ -13,12 +14,16 @@ if __name__ == '__main__':
 
     r = None
     classes = ["Black", "Red", "Green", "Blue"]
-    prev_class = None
-    Next_class = None
-
+    prev_class = 0
+    next_class = None
+    ct = 0
     cap = cv2.VideoCapture(0)
 
+    # State variables
+
     flag = 1
+    ts = 0
+
     while True:
         # Capture frame
         ret, frame = cap.read()
@@ -29,17 +34,45 @@ if __name__ == '__main__':
 
         # Perform operations on the frame
 
-
          # Display the frame
-        time.sleep(0.5)
+
         mean_int = cv2.mean(roi)
+        if np.linalg.norm(mean_int) < 150:
+            next_class = 0
+            print("Black")
+        elif np.linalg.norm(mean_int[:2]) < mean_int[2]:
+            next_class = 1
+            print("Red")
+        elif np.linalg.norm(mean_int[1:3]) < mean_int[0]:
+            next_class = 2
+            print("Blue")
+        else:
+            next_class = 3
+            print("Green")
 
-        print(mean_int)
+        if not ts:
+            if prev_class == 0:
+                if next_class == 1:
+                    print("Tranmsission started")
+                    m1 = "1"
+                    ts = 1
+                if next_class == 2:
+                    print("Tranmsission started")
+                    m1 = "0"
+                    ts = 1
+        else:
+            if prev_class == 3 and next_class == 1:
+                m1 = m1 + "1"
+            elif prev_class == 3 and next_class == 2:
+                m1 = m1 + "0"
 
-        cv2.imshow('frame', roi)
+        prev_class = next_class
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
+        elif ct > 10:
+            break
+        print(m1)
     # When everything done, release the capture
+
     cap.release()
     cv2.destroyAllWindows()
--- vision/send.py
+++ vision/send.py
@@ -1,15 +1,51 @@
 import cv2
 import time
 import numpy as np
-from transmit import transmit
+
+
+def transmit(msg):
+    for i in range(len(msg)):
+        x = msg[-(i + 1)]
+        if x == '1':
+            pass
+            display(1)
+        else:
+            pass
+            display(0)
+        display("Green")
+        cv2.waitKey(200)
+
+def display(col):
+    global red_img
+    global blue_img
+    global green_img
+    global win
+    cv2.namedWindow('display', cv2.WINDOW_FULLSCREEN)
+    if col == 1:
+        cv2.imshow('display', red_img)
+        cv2.waitKey(1000)
+        print("Displaying red")
+    elif col == 0:
+        cv2.imshow('display', blue_img)
+        cv2.waitKey(1000)
+        print("Displaying blue")
+    else:
+        cv2.imshow('display', green_img)
+        cv2.waitKey(500)
+
+
+
 
 def encode(message, pos1, pos2):
     pass
 
-def transmit(msg):
-    pass
-
 if __name__ == '__main__':
+
+    # Load images
+    red_img = cv2.imread("red.jpg")
+    blue_img = cv2.imread("blue.jpg")
+    green_img = cv2.imread("green.jpg")
+    black_img = np.zeros(red_img.shape)
 
     # first message
     m1 = input("Enter first message to be sent: ")
