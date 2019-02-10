049494ccdaac2a8e709f59c1a71631a4f4a31e51
kansalaman <amankansal.india@gmail.com>
2019-02-10 13:51:18 +0530

    increased speed

--- application/send.py
+++ application/send.py
@@ -24,15 +24,15 @@ def display(col):
     cv2.namedWindow('display', cv2.WINDOW_FULLSCREEN)
     if col == 1:
         cv2.imshow('display', red_img)
-        cv2.waitKey(1000)
+        cv2.waitKey(bit_duration)
         print("Displaying red")
     elif col == 0:
         cv2.imshow('display', blue_img)
-        cv2.waitKey(1000)
+        cv2.waitKey(bit_duration)
         print("Displaying blue")
     else:
         cv2.imshow('display', green_img)
-        cv2.waitKey(1000)
+        cv2.waitKey(bit_duration)
 
 
 def start():
@@ -45,9 +45,9 @@ def start():
 def sep():
     cv2.namedWindow('display', cv2.WINDOW_FULLSCREEN)
     cv2.imshow('display', red_img)
-    cv2.waitKey(500)
+    cv2.waitKey(bit_duration)
     cv2.imshow('display', blue_img)
-    cv2.waitKey(500)
+    cv2.waitKey(bit_duration)
     print("Displaying sep character")
 
 
@@ -58,15 +58,9 @@ def black():
     print("Displaying black")
 
 
-def end():
-    cv2.namedWindow('display', cv2.WINDOW_FULLSCREEN)
-    cv2.imshow('display', blue_img)
-    cv2.waitKey(1000)
-    cv2.imshow('display', red_img)
-    cv2.waitKey(1000)
-    print("Displaying end character")
 
 if __name__ == '__main__':
+    bit_duration = int(input("Enter bit duration: "))
     # Load images
     red_img = cv2.imread("red.jpg")
     blue_img = cv2.imread("blue.jpg")
